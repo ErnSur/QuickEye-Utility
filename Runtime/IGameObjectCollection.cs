f42b244bf5d9a3483dd5b90ad510aa85f4d37b0c
Ernest Suryś <ernest.surys@gmail.com>
2020-04-16 11:14:31 +0200

    ElementList rename and refinement

---
+++ Runtime/IGameObjectCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace QuickEye.CanvasElements
+{
+    /// <summary>
+    /// A collection of gameobjects
+    /// where a lifetime of the gameobject is bound to the collection
+    /// if its removed from the collection it should no longer be active in the scene
+    /// </summary>
+    public interface IGameObjectCollection<T> : ICollection<T> where T : Component
+    {
+        T AddNew();
+    }
+}
