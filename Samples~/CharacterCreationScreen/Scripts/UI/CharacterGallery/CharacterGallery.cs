fdc301722bc3b6a3e552794684493fccd65fe74a
Ernest Suryś <ernest.surys@gmail.com>
2020-05-16 14:03:16 +0200

    Updated samples and removed logs

---
+++ Samples~/CharacterCreationScreen/Scripts/UI/CharacterGallery/CharacterGallery.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace QuickEye.Utility.CharacterCreation
+{
+
+    public class CharacterGallery : CanvasElement<(CharacterCreationEvents events, CharacterTemplate[] fighters)>
+    {
+        [SerializeField]
+        private CharacterGalleryItems _galleryItems;
+
+        public override void Initialize((CharacterCreationEvents events, CharacterTemplate[] fighters) c)
+        {
+            base.Initialize(c);
+            
+            foreach (var item in c.fighters)
+            {
+                _galleryItems.AddNew().Initialize((c.events, item));
+            }
+        }
+    }
+}
