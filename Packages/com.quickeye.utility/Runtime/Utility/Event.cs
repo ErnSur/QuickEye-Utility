d4ab3c5b24d6703e8ad5be13b0d7465841ddae40
Ernest Suryś <ernest.surys@gmail.com>
2021-12-01 22:07:34 +0100

    Added LastPayload to event class

--- Packages/com.quickeye.utility/Runtime/Utility/Event.cs
+++ Packages/com.quickeye.utility/Runtime/Utility/Event.cs
@@ -2,19 +2,22 @@ using System;
 
 namespace QuickEye.Utility
 {
-    public abstract class Event<T>  where T : Event<T>
+    public abstract class Event<T> where T : Event<T>
     {
         public static event Action<T> Ev;
+        public static T LastPayload { get; private set; }
         public static void Register(Action<T> callback) => Ev += callback;
         public static void Unregister(Action<T> callback) => Ev -= callback;
-        public static void Trigger(T payload) => Ev?.Invoke(payload);
+        public static void Trigger(T payload) => Ev?.Invoke(LastPayload = payload);
     }
 
-    public abstract class Event<T,TArgs>  where T : Event<T,TArgs>
+    public abstract class Event<T, TArgs> where T : Event<T, TArgs>
     {
         public static event Action<TArgs> Ev;
+        public static TArgs LastPayload { get; private set; }
+
         public static void Register(Action<TArgs> callback) => Ev += callback;
         public static void Unregister(Action<TArgs> callback) => Ev -= callback;
-        public static void Trigger(TArgs payload) => Ev?.Invoke(payload);
+        public static void Trigger(TArgs payload) => Ev?.Invoke(LastPayload = payload);
     }
 }
