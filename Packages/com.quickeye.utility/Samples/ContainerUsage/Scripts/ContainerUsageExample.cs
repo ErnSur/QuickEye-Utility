3a2f347ee84c6d6d60dc53d4ba6d31a6ac3551b2
Ernest Suryś <ernest.surys@gmail.com>
2022-05-04 21:23:02 +0200

    Updated samples

---
+++ Packages/com.quickeye.utility/Samples/ContainerUsage/Scripts/ContainerUsageExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using QuickEye.Utility;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QuickEye.Samples.ContainerUsage
+{
+    public class ContainerUsageExample : MonoBehaviour
+    {
+        [SerializeField]
+        private PictureGallery pictureGallery;
+
+        [SerializeField]
+        private Sprite[] pictures;
+
+        private void Awake()
+        {
+            SetupGallery();
+        }
+
+        public void UpdatePictures(Sprite[] newPictures)
+        {
+            pictures = newPictures;
+            SetupGallery();
+        }
+
+        public void UpdatePicturesColor(Color color)
+        {
+            foreach (var image in pictureGallery)
+                image.color = color;
+        }
+
+        private void SetupGallery()
+        {
+            pictureGallery.Clear();
+            foreach (var picture in pictures)
+                pictureGallery.AddNew().sprite = picture;
+        }
+        
+        [Serializable]
+        public class PictureGallery : Container<Image> { }
+    }
+}
