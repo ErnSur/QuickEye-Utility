bfe57b47d453f1bedef5289f18bef388ce8d95ed
Ernest Suryś <ernest.surys@gmail.com>
2022-05-21 11:47:47 +0200

    added new menu items
    - select empty folders
    - Open manifest.json

---
+++ Packages/com.quickeye.utility/Editor/OpenPackagesManifestMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace QuickEye.Utility.Editor
+{
+    internal static class OpenPackagesManifestMenuItem
+    {
+        [MenuItem("Assets/Open Packages Manifest")]
+        private static void OpenManifest()
+        {
+            EditorUtility.OpenWithDefaultApp("Packages/manifest.json");
+        }
+    }
+}
