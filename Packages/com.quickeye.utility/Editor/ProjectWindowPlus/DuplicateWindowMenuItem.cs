008f8ee408c55ea1c46e595aea1212802a8a404e
Ernest Suryś <ernest.surys@gmail.com>
2022-05-23 17:53:28 +0200

    Squashed commit of the following:
    commit 519cc7a519f52d0fe14b0123f1ba6435a51101cc
    Author: Ernest Suryś <ernest.surys@gmail.com>
    Date:   Mon May 23 17:51:52 2022 +0200
    
        updated harmony version
    
    commit a5db75282bfb6a2b550bc14d831058633274898d
    Author: Ernest Suryś <ernest.surys@gmail.com>
    Date:   Sat May 21 19:39:01 2022 +0200
    
        Added Duplicate Window menu item
    
    commit f2fe294aceefb0aa3382e567abdf519f10fd0c04
    Author: Ernest Suryś <ernest.surys@gmail.com>
    Date:   Sat May 21 12:03:38 2022 +0200
    
        added harmony dll

---
+++ Packages/com.quickeye.utility/Editor/ProjectWindowPlus/DuplicateWindowMenuItem.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace QuickEye.Utility.Editor
+{
+    public static class DuplicateWindowMenuItem
+    {
+        [MenuItem("Window/Duplicate window %#D", priority = 10)]
+        public static void DuplicateWindow()
+        {
+            var window = EditorWindow.focusedWindow;
+            var newWindow = UnityEngine.Object.Instantiate(window);
+
+            TryLockWindow(newWindow);
+
+            newWindow.Show();
+            newWindow.position = GetRectNextTo(window.position);
+        }
+
+        private static void TryLockWindow(EditorWindow newWindow)
+        {
+            var windowType = newWindow.GetType();
+            if (EditorWindowLocker.TryGetWindowLocker(windowType, out var locker))
+                locker.LockWindow(newWindow, true);
+        }
+
+        private static Rect GetRectNextTo(Rect rect)
+        {
+            return new Rect
+            {
+                x = Mathf.Max(rect.position.x - rect.size.x, 0),
+                y = rect.position.y,
+                size = rect.size
+            };
+        }
+    }
+
+    internal class EditorWindowLocker
+    {
+        private const string LockTrackerLockedPropertyName = "isLocked";
+
+        private static readonly Type _LockerType = typeof(EditorGUIUtility).Assembly.GetTypes()
+            .FirstOrDefault(t => t.Name == "EditorLockTracker");
+
+        private static readonly Dictionary<Type, EditorWindowLocker> _CachedLockers =
+            new Dictionary<Type, EditorWindowLocker>();
+
+        public static bool TryGetWindowLocker(Type windowType, out EditorWindowLocker locker)
+        {
+            if (_CachedLockers.TryGetValue(windowType, out locker))
+                return locker != null;
+
+            if (typeof(EditorWindow).IsAssignableFrom(windowType) && GetLockTrackerField(windowType) != null)
+                locker = new EditorWindowLocker(windowType);
+
+            _CachedLockers[windowType] = locker;
+            return locker != null;
+        }
+
+        // SceneHierarchyWindow needs special handling, it's not worth it
+        private static FieldInfo GetLockTrackerField(Type windowType)
+        {
+            var fields = windowType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            var lockerField = fields.FirstOrDefault(f => _LockerType.IsAssignableFrom(f.FieldType));
+            return lockerField;
+        }
+
+        private readonly FieldInfo _lockTrackerField;
+        private readonly PropertyInfo _isLockedProperty;
+
+        private EditorWindowLocker(Type windowType)
+        {
+            _lockTrackerField = GetLockTrackerField(windowType);
+            _isLockedProperty = _lockTrackerField.FieldType.GetProperty(LockTrackerLockedPropertyName,
+                BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        public void LockWindow(EditorWindow window, bool isLocked)
+        {
+            try
+            {
+                _isLockedProperty.SetValue(_lockTrackerField.GetValue(window), isLocked);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
