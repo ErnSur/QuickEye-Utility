a652cc06cdb0dcfb1a7f959e4bab50f43cc4ca64
Ernest Suryś <ernest.surys@gmail.com>
2022-05-23 17:53:28 +0200

    Squashed commit of the following:
    commit 519cc7a519f52d0fe14b0123f1ba6435a51101cc
    Author: Ernest Suryś <ernest.surys@gmail.com>
    Date:   Mon May 23 17:51:52 2022 +0200
    
        updated harmony version
    
    commit a5db75282bfb6a2b550bc14d831058633274898d
    Author: Ernest Suryś <ernest.surys@gmail.com>
    Date:   Sat May 21 19:39:01 2022 +0200
    
        Added Duplicate Window menu item
    
    commit f2fe294aceefb0aa3382e567abdf519f10fd0c04
    Author: Ernest Suryś <ernest.surys@gmail.com>
    Date:   Sat May 21 12:03:38 2022 +0200
    
        added harmony dll

---
+++ Packages/com.quickeye.utility/Editor/Harmony/HarmonyPatcher.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEditor;
+
+namespace QuickEye.Utility.Editor
+{
+
+    public static class HarmonyPatcher
+    {
+        public static Harmony Harmony;
+
+        [InitializeOnLoadMethod]
+        public static void DoPatching()
+        {
+            Harmony = new Harmony("com.quickeye.utility");
+        }
+    }
+}
