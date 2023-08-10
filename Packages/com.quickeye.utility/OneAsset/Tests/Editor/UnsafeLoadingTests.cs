25c6665064595e2f642e8e0e3a222ef9ee0e7c8d
Ernest Suryś <ernest.surys@gmail.com>
2023-08-10 22:11:31 +0200

    CR changes

--- Packages/com.quickeye.utility/OneAsset/Tests/Editor/UnsafeLoadingTests.cs
+++ Packages/com.quickeye.utility/OneAsset/Tests/Editor/UnsafeLoadingTests.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using NUnit.Framework;
 using OneAsset.Editor.Tests.SampleAssets;
 using UnityEngine;
@@ -6,16 +7,30 @@ namespace OneAsset.Editor.Tests
 {
     public class UnsafeLoadingTests
     {
+        private const string FilePath = "OneAsset_LoadAndForget_Test";
+
+        [SetUp]
+        public void SetUp()
+        {
+            var so = ScriptableObject.CreateInstance<SoWithAsset>();
+            UnityEditorInternal.InternalEditorUtility.SaveToSerializedFileAndForget(new Object[] { so }, FilePath,
+                true);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (File.Exists(FilePath))
+                File.Delete(FilePath);
+        }
+
         [Test]
         public void LoadUnsafe()
         {
-            var options = new AssetLoadOptions("UNSAFE_TEST")
+            var options = new AssetLoadOptions(FilePath)
             {
                 LoadAndForget = true
             };
-            var so = ScriptableObject.CreateInstance<SoWithAsset>();
-            UnityEditorInternal.InternalEditorUtility.SaveToSerializedFileAndForget(new Object[] { so }, options.Paths[0],
-                true);
 
             var instance = OneAssetLoader.LoadOrCreateScriptableObject(typeof(SoWithAsset), options);
 
