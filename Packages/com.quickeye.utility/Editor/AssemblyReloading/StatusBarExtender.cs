9b3744bb3f1602aee9490c659f8375b66410ddcc
Ernest Suryś <ernest.surys@gmail.com>
2024-01-05 21:58:06 +0100

    added fail guard

--- Packages/com.quickeye.utility/Editor/AssemblyReloading/StatusBarExtender.cs
+++ Packages/com.quickeye.utility/Editor/AssemblyReloading/StatusBarExtender.cs
@@ -9,8 +9,10 @@ namespace QuickEye.Utility.Editor.AssemblyReloading
 {
     internal static class StatusBarExtender
     {
+        private const int MaxRetries = 5;
         public static event Action<VisualElement> StatusBarCreated; 
         private static GUIView _statusBarInstance;
+        private static int _retries;
         
         [InitializeOnLoadMethod]
         private static void Initialize()
@@ -20,18 +22,29 @@ namespace QuickEye.Utility.Editor.AssemblyReloading
 
         private static void OnUpdate()
         {
-            if (_statusBarInstance != null)
+            if (_statusBarInstance != null || _retries >= MaxRetries)
                 return;
             _statusBarInstance = Resources.FindObjectsOfTypeAll<AppStatusBar>().FirstOrDefault();
             if(_statusBarInstance == null)
-                return; // todo: prevent FindObjectsOfTypeAll on every tick
-                
-            var visualTree = _statusBarInstance.windowBackend.visualTree as VisualElement;
-            visualTree.styleSheets.Add(UIElementsEditorUtility.GetCommonDarkStyleSheet());
+            {
+                _retries++;
+                return;
+            }
 
-            var originalBar = visualTree.Q<IMGUIContainer>();
-            originalBar.AddToClassList("status-bar");
-            StatusBarCreated?.Invoke(visualTree);
+            try
+            {
+                var visualTree = _statusBarInstance.windowBackend.visualTree as VisualElement;
+                visualTree.styleSheets.Add(UIElementsEditorUtility.GetCommonDarkStyleSheet());
+
+                var originalBar = visualTree.Q<IMGUIContainer>();
+                originalBar.AddToClassList("status-bar");
+                StatusBarCreated?.Invoke(visualTree);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Failed to extend status bar: {e}");
+                _retries = MaxRetries;
+            }
         }
     }
 }
